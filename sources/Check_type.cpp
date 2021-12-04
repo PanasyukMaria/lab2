88bdbcfb144b9676f80255123e62b11d9311226d
morgan <marymorgan7815@gmail.com>
2021-12-05 01:02:47 +0300

    labrab

--- sources/Check_type.cpp
+++ sources/Check_type.cpp
@@ -3,20 +3,6 @@
 #include "Check.h"
 #include <sstream>
 
-std::ostream & operator<<(std::ostream & os, Check_type const & type) {
-  switch (type) {
-    case _random:
-      os << "_random";
-      break;
-    case _forward:
-      os << "_forward";
-      break;
-    case _reverse:
-      os << "_reverse";
-      break;
-  }
-  return os;
-}
 std::string to_string(Check_type type)
 {
   std::stringstream ss;
